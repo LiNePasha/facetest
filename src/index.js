5e1d6a720608cce49c3558016c9ec227fad5e242
AhmedWaled <67336269+LiNePasha@users.noreply.github.com>
2022-12-27 18:07:08 +0200

    upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+// 
+// "@tensorflow-models/coco-ssd": "0.1.1",
+// "@tensorflow/tfjs": "0.14.0",
+// "react-magic-dropzone": "1.0.1",
